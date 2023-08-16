c13835e41281c019504a8acb7eb0a3972059b33a
Saoul DImbanda <saouldimbanda@MacBook-Pro-de-Saoul.local>
2023-08-16 16:35:38 +0200

    delete comment index.js

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-/*
-
-const express = require("express");
-
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cloudinary = require("cloudinary").v2;
-require("dotenv").config();
-
-mongoose.connect("mongodb://localhost/vinted-user");
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use(express.json());
-
-cloudinary.config({
-  cloud_name: "dbbszpcem",
-  api_key: "152421262498747",
-  api_secret: "Ibn2ckCSppFAQpS5at5pW_cANiQ",
-});
-
-const offerRoutes = require("./routes/offer");
-
-app.use(offerRoutes);
-
-app.listen(3000, (req, res) => {
-  console.log("server is Ok");
-});
-
-*/
-
 const express = require("express");
 const app = express();
 require("dotenv").config();
